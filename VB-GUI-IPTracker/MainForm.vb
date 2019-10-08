e83cfa7d0c05095343eb9c9cdbdce39e1ccf0baa
Barry Gaffey <bagaffey@users.noreply.github.com>
2019-10-07 22:06:14 -0400

    Text will turn red after the IP has already been tracked.

--- VB-GUI-IPTracker/MainForm.vb
+++ VB-GUI-IPTracker/MainForm.vb
@@ -1,5 +1,8 @@
 ﻿Public Class MainForm
     Private Sub btnTrackIP_Click(sender As Object, e As EventArgs) Handles btnTrackIP.Click
+        If Not String.IsNullOrWhiteSpace(txtKillersIP.Text) Then
+            txtKillersIP.ForeColor = Color.Red
+        End If
         txtKillersIP.Text = "127.0.0.1"
     End Sub
 End Class
